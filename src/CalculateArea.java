070d71a2338ea35c5f383d76068707ce00fce088
twila <twiladzbanski@hotmail.com>
2019-09-18 13:48:49 -0500

    just checking

---
+++ src/CalculateArea.java
@@ -0,0 +1,9 @@
+public class CalculateArea
+    public static void main(int number1) {
+        int a = 10;
+        int b = 20;
+        int area = number1 * b;
+        System.out.println("The Area is : " + area);
+    }
+
+}
